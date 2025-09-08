5667faf6cf2031a4ae55ce4fbaa9b2851af13f95
Andrew Stevenson <129066987+AndrewStevenson550@users.noreply.github.com>
2025-09-08 13:09:27 +0000

    added the code button which added a click every second

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const clanker_id = document.getElementById("clanker");
 const money_id = document.getElementById("money");
 const clear = document.getElementById("button-clear");
+const code = document.getElementById("code")
+
+
+
 // Load saved clicks from localStorage, or start at 0
 let clicks = Number(localStorage.getItem("clicks")) || 0;
 money_id.innerHTML = clicks;
@@ -21,3 +25,21 @@ clear.addEventListener("click", function(){
     clicks = 0
     money_id.innerHTML = 0
 })
+
+
+//____________________
+
+
+
+//code stuff (the clicker)
+
+let autoInterval = null; // keep track of the timer so it doesn't restart
+code.addEventListener("click", () => {
+  if (autoInterval === null) {
+    autoInterval = setInterval(() => {
+      clicks++; // add 1 automatically
+      money.textContent = clicks; // update display
+      localStorage.setItem("clicks", clicks); // save progress
+    }, 2000); // 2000 ms = 2 seconds
+  }
+});
